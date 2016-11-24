aa4953a3b2cb2aef247a8fdd83ea589431eb40f9
lynas <szlynas@gmail.com>
2016-11-25 00:01:17 +0600

    interceptor and dev tools added

--- src/main/kotlin/com/lynas/SpringBootNeo4jApplication.kt
+++ src/main/kotlin/com/lynas/SpringBootNeo4jApplication.kt
@@ -1,16 +1,19 @@
 package com.lynas
 
+import com.lynas.config.InterceptorConfig
 import org.neo4j.ogm.config.Configuration
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @EnableTransactionManagement
 @EnableNeo4jRepositories
 @SpringBootApplication
-open class SpringBootNeo4jApplication{
+open class SpringBootNeo4jApplication(val interceptorConfig: InterceptorConfig) : WebMvcConfigurerAdapter() {
     @Bean
     open fun getConfiguration(): Configuration {
         val config = Configuration()
@@ -19,8 +22,19 @@ open class SpringBootNeo4jApplication{
                 .setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver").uri = "http://neo4j:123456@localhost:7474"
         return config
     }
+
+
+    override fun addInterceptors(registry: InterceptorRegistry?) {
+        super.addInterceptors(registry)
+        registry?.addInterceptor(interceptorConfig)
+    }
 }
 
 fun main(args: Array<String>) {
     SpringApplication.run(SpringBootNeo4jApplication::class.java, *args)
+}
+
+
+object AppConstant {
+    val organization = "ORGANIZATION"
 }
---
+++ src/main/kotlin/com/lynas/config/InterceptorConfig.kt
@@ -0,0 +1,26 @@
+package com.lynas.config
+
+import com.lynas.AppConstant
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * Created by sazzad on 10/3/16
+ */
+
+@Component
+open class InterceptorConfig : HandlerInterceptorAdapter() {
+
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any?): Boolean {
+        when (request.requestURI) {
+            "/", "/login", "/logout" -> return true
+        }
+        if (null == request.session.getAttribute(AppConstant.organization)) {
+            response.sendRedirect("/")
+            return false
+        }
+        return true
+    }
+}
